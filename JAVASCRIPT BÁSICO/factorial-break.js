31145e049cfee00b5487d6ff6f28683341a880ba
Jonathan L <45992049+jlriesgo@users.noreply.github.com>
2022-11-14 19:17:19 +0100

    Add files via upload

---
+++ JAVASCRIPT BÁSICO/factorial-break.js
@@ -0,0 +1,14 @@
+let numero = 10;
+
+let numero_inicial = 1;
+
+while (numero_inicial < 10) {
+    console.log(numero *= numero_inicial)
+    numero_inicial++;
+    
+    if(numero == 1200){
+        console.log("llegó a 1200");
+        break;
+    }
+    
+}
---
+++ JAVASCRIPT BÁSICO/factorial-for.js
@@ -0,0 +1,5 @@
+let numero = 10;
+
+for(let i=1; i < 10; i++){
+    console.log(numero*=i)
+}
