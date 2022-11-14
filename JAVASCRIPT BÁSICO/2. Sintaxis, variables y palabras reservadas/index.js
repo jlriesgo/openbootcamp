1ad44cfaa65d78cf23334a6e581447477f7bfab3
jlriesgo <45992049+jlriesgo@users.noreply.github.com>
2022-11-14 13:46:01 +0100

    Add files via upload

--- JAVASCRIPT BÁSICO/2. Sintaxis, variables y palabras reservadas/index.js
+++ JAVASCRIPT BÁSICO/2. Sintaxis, variables y palabras reservadas/index.js
@@ -1,1 +1,18 @@
+let nombre = "Jonathan";
+let edad = 39;
+let isDesarrollador = true;
+let fecha_nacimiento = new Date("1983 april 08");
 
+console.log(fecha_nacimiento)
+
+let libro_favorito = {
+    titulo : "Reina Roja",
+    autor : "Juan Gómez Jurado",
+    fecha : "abril 2019",
+    url : "https://amazon.es"
+
+}
+
+var informacion = [nombre, edad, isDesarrollador, fecha_nacimiento, libro_favorito]
+
+console.log(informacion)
