06fc5757bd4e50315ebfa8f8f8bda6da1d2b33da
Jonathan L <45992049+jlriesgo@users.noreply.github.com>
2022-11-15 18:11:03 +0100

    Add files via upload

--- JAVASCRIPT BÁSICO/6. Listas/listas.js
+++ JAVASCRIPT BÁSICO/6. Listas/listas.js
@@ -1,1 +1,46 @@
+let lista_compra = ["leche", "yogures","huevos","detergente","jamon de pavo"]
 
+lista_compra.push("Aceite de girasol")
+
+console.log(lista_compra)
+
+lista_compra.pop()
+
+console.log(lista_compra)
+
+let peliculas_favoritas = [{
+    titulo : "Aterriza como puedas",
+    director : "Ken Finkleman",
+    fecha : 1980
+},
+{
+    titulo : "Vengadores: La era de Ultrón",
+    director : "Joss Whedon",
+    fecha : 2015
+},
+{
+    titulo : "La lista de Schindler",
+    director : "Steven Spielberg",
+    fecha : 1993
+}
+]
+
+let peliculasposteriores = peliculas_favoritas.filter( f => f.fecha > 2010)
+
+console.log(peliculasposteriores)
+
+let directores = peliculas_favoritas.map(d => d.director)
+
+console.log(directores)
+
+let titulos = peliculas_favoritas.map(t => t.titulo);
+
+console.log(titulos)
+
+let directores_titulos = directores.concat(titulos);
+
+console.log(directores_titulos)
+
+let directores_titulos_propag = [...titulos, ...directores]
+
+console.log(directores_titulos_propag)
