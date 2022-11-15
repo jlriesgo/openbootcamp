056f25133d3290bb72a82f4c0fdc595b7870ad51
Jonathan L <45992049+jlriesgo@users.noreply.github.com>
2022-11-15 11:15:51 +0100

    Add files via upload

--- JAVASCRIPT BÁSICO/4. Strings/index.js
+++ JAVASCRIPT BÁSICO/4. Strings/index.js
@@ -1,1 +1,31 @@
+let nombre = "Jonathan";
 
+let apellido = "León"
+
+let estudiante = `${nombre} ${apellido}`
+
+console.log(estudiante)
+
+console.log(estudiante.toUpperCase())
+
+console.log(estudiante.toLowerCase())
+
+let num_letras = estudiante.length;
+
+console.log(num_letras)
+
+let primera_letra = nombre.charAt(0)
+
+console.log(primera_letra)
+
+let ultima_letra = apellido.charAt(3);
+
+console.log(ultima_letra)
+
+let elimina_espacio = estudiante.replace(" ","");
+
+console.log(elimina_espacio)
+
+let isEstudiante = estudiante.includes("Jonathan");
+
+console.log(isEstudiante)
