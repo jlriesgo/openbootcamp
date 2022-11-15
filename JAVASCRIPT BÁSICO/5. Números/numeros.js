5a6970a074ba126df229d975620bdaf4398562b9
Jonathan L <45992049+jlriesgo@users.noreply.github.com>
2022-11-15 12:10:20 +0100

    Add files via upload

--- JAVASCRIPT BÁSICO/5. Números/numeros.js
+++ JAVASCRIPT BÁSICO/5. Números/numeros.js
@@ -1,1 +1,17 @@
+let altura = 173;
 
+let altura_float = 1.73;
+
+let peso_float = 80.4;
+
+let altura_redondeo = parseFloat(altura_float);
+
+console.log(altura_redondeo)
+
+let peso_redondeo = parseInt(peso_float);
+
+console.log (peso_redondeo)
+
+var max_valor = 83;
+
+console.log(Number.MAX_VALUE);
