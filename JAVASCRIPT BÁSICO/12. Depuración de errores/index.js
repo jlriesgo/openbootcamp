f0a17d061e021bafdc751e0caadb64dc2f984818
Jonathan L <45992049+jlriesgo@users.noreply.github.com>
2022-11-22 13:28:36 +0100

    Add files via upload

--- JAVASCRIPT BÁSICO/12. Depuración de errores/index.js
+++ JAVASCRIPT BÁSICO/12. Depuración de errores/index.js
@@ -1,1 +1,15 @@
 
+function fibonnacci(num){
+    let listaNum = [0,1];
+
+    for(let i = 2; i <= num; i++){
+        listaNum.push((listaNum[i-1])+ (listaNum[i-2]))  
+        
+    }
+
+    return [listaNum, listaNum[num]]
+}
+
+console.log(fibonnacci(10)[0])
+
+console.log(fibonnacci(10)[1])
