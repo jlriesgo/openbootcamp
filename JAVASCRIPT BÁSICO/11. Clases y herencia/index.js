7788264300fc2e6266b498b9dd5cb8f4956297ea
Jonathan L <45992049+jlriesgo@users.noreply.github.com>
2022-11-22 12:46:07 +0100

    Add files via upload

--- JAVASCRIPT BÁSICO/11. Clases y herencia/index.js
+++ JAVASCRIPT BÁSICO/11. Clases y herencia/index.js
@@ -1,1 +1,16 @@
+class Estudiante {
+    #nombre
+    constructor(nombre){
+        this.#nombre = nombre;
+        this.asignaturas = ['JavaScript','HTML','CSS']
+    }
 
+    obtenDatos(){
+        console.log(`El nombre del alumno es ${this.#nombre} y sus asignaturas son ${this.asignaturas}`)
+    }
+    
+}
+
+const estudiante = new Estudiante('María')
+
+estudiante.obtenDatos()
