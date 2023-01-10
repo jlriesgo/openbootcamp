00f4b7b80c6001824ac6436b995e20874b014824
Jonathan L <45992049+jlriesgo@users.noreply.github.com>
2023-01-10 09:07:19 +0100

    Add files via upload

---
+++ REACT/07. Ejercicio sesiones 19, 20 y 21/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ChuckNorrisJokes from './components/pure/ChuckNorrisJokes';
+
+function App() {
+  return (
+    <div className="App">
+      <ChuckNorrisJokes></ChuckNorrisJokes>
+    </div>
+  );
+}
+
+export default App;
